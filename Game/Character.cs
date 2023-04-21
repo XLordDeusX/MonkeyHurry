835ac491302a5d8dc24c80bfe8af6d8e5e401c9a
MariaERodriguez <mariaeugeniarodriguez2002@gmail.com>
2023-04-21 15:49:38 -0300

    Mis cambios

---
+++ Game/Character.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    //This part is related to the main character
+    public class Character
+    {
+        private string image;
+        private Transform transform;
+        private RendereableObject renderComponent;
+
+        private float speedX;
+        private float speedY;
+
+        public Character(string p_image, float p_posX, float p_posY, float p_speedX, float p_speedY)
+        {
+            image = p_image;
+            transform = new Transform(p_posX, p_posY);
+            renderComponent = new RendereableObject(p_image);
+            speedX = p_speedX;
+            speedY = p_speedY;
+        }
+
+        public void Update()
+        {
+            transform.Move(speedX * Program.deltaTime, speedY * Program.deltaTime); 
+        }
+
+        public void Render()
+        {
+            renderComponent.Render(transform.PosX, transform.PosY,transform.ScaleX,transform.ScaleY,transform.Rot,transform.OffsetX,transform.OffsetY);
+        }
+    }
+}
